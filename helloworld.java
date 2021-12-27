6e0186af843106f2e02b3ffc90c3ac5d16b17e21
mobellaq <93530388+mobellaq@users.noreply.github.com>
2021-12-27 09:26:39 +0100

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld 
+{
+ 
+       public static void main (String[] args)
+       {
+             // Ausgabe Hello World!
+             System.out.println("Hello World!");
+             System.out.print("Alhamdulilah");
+       }
+}
